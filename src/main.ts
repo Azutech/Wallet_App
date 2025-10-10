14968ce07fcc2b479e8ab1e78bb430c0a28b6800
Azutech <azunna.onugha@gmail.com>
2025-10-10 22:56:10 +0100

    feat: initialize wallet application with NestJS framework
    - Add package.json with dependencies and scripts for the project
    - Create AppController and AppService with a simple hello world endpoint
    - Implement AppModule to organize application structure
    - Add HTTP logger middleware for request logging
    - Set up main entry point for the application with CORS and global prefix
    - Create unit tests for AppController and e2e tests for the application
    - Add Jest configuration for e2e testing
    - Configure TypeScript settings for building the application

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { HttpLogger } from './common/middleware/http-logger.middleware';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('Wallet-Ops-V1');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('PORT');
+  app.setGlobalPrefix('api/v1');
+  app.use(new HttpLogger().use);
+
+  app.set('trust proxy', 1); // trust first proxy
+
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+    credentials: true,
+    maxAge: 3600,
+  });
+
+  // Port
+  await app.listen(port, () => logger.log(`App running on Port: ${port}`));
+}
+bootstrap();
