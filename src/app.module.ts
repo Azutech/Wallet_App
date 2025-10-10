9fd8833e94e8910eb587cb0aab9d4474d24a44fa
Azutech <azunna.onugha@gmail.com>
2025-10-10 23:00:08 +0100

    feat: add global configuration module for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
