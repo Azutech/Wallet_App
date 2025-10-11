7a15483f6b06553290741bbc5b7dbd9ff2fda8c8
Azutech <azunna.onugha@gmail.com>
2025-10-11 11:48:00 +0100

    feat: add TypeORM and PostgreSQL support
    - Updated package.json to include @nestjs/typeorm and pg dependencies.
    - Modified app.module.ts to import TypeOrmModule (commented out configuration for PostgreSQL).

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -10,6 +11,17 @@ import { AppService } from './app.service';
       isGlobal: true,
       expandVariables: true,
     }),
+
+    //    TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',       // your postgres host
+    //   port: 5432,              // default postgres port
+    //   username: 'youruser',    // your postgres username
+    //   password: 'yourpassword',// your postgres password
+    //   database: 'yourdbname',  // your postgres database name
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,       // use only in dev; auto create tables
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
